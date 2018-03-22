dae716a858361d961cabca5afcba0767ac6da238
wenbo <wenbolovesnz@gmail.com>
2018-03-22 15:23:06 +1300

    dev and pro ready webpack 4 setup

---
+++ src/index.js
@@ -0,0 +1,5 @@
+console.log('hello world');
+const arr = [1, 2, 3];
+const iAmJavascriptES6 = () => console.log(...arr);
+window.iAmJavascriptES6 = iAmJavascriptES6;
+iAmJavascriptES6();
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); 
+
+module.exports = {
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader"
+            }
+        }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin()
+  ]
+};
